d2073f753e0eb370b5b97ac2a86d694c4e650183
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2025-11-06 14:46:39 +0100

    Subida de ft_printf y get_next_line

---
+++ Cursus/get_next_line/get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarcos- <pmarcos-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 15:43:53 by pmarcos-          #+#    #+#             */
+/*   Updated: 2025/11/05 17:32:55 by pmarcos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// Auxiliars functions
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+
+// New functions
+char	*get_next_line(int fd);
+char	*extract_line(char *str);
+void	handle_exceptions(int fd, char *buffer, char *bytes_read);
+
+#endif
