0fea3681e84bff1efc78fd8be861951c94478612
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-02-16 20:17:29 +0100

    Add files via upload

--- Cursus/milestone2/exam_milestone2/level2/union.c
+++ Cursus/milestone2/exam_milestone2/level2/union.c
@@ -6,11 +6,49 @@
 /*   By: pmarcos- <pmarcos-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 20:14:24 by pmarcos-          #+#    #+#             */
-/*   Updated: 2026/01/29 12:18:54 by pmarcos-         ###   ########.fr       */
+/*   Updated: 2026/02/16 19:52:23 by pmarcos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main()
-{
+#include <unistd.h>
 
+int	seen_before(char *str, int end, char c)
+{
+	int	i = 0;
+	while (i < end)
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	if (argc == 3)
+	{
+		j = 1;
+		while (j <= 2)
+		{
+			i = 0;
+			while (argv[j][i])
+			{
+				c = argv[j][i];
+				if (!seen_before(argv[j], i, c))
+				{
+					if (j == 1 || !seen_before(argv[1], __INT_MAX__, c))
+						write(1, &c, 1);
+				}
+				i++;
+			}
+			j++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
--- Cursus/milestone2/exam_milestone2/level2/wdmatch.c
+++ Cursus/milestone2/exam_milestone2/level2/wdmatch.c
@@ -6,8 +6,32 @@
 /*   By: pmarcos- <pmarcos-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 18:25:57 by pmarcos-          #+#    #+#             */
-/*   Updated: 2026/01/28 18:28:26 by pmarcos-         ###   ########.fr       */
+/*   Updated: 2026/02/16 20:16:30 by pmarcos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+int	main(int argc, char **argv)
+{
+	char *str1 = argv[1];
+	char *str2 = argv[2];
+	int	size = 0;
+	int cnt = 0;
+
+	if (argc != 3)
+		write(1, "\n", 1);
+	else if (argc == 3)
+	{
+		while (str1[size] != '\0')
+			size++;
+		while (cnt < size && *str2)
+		{
+			if (str1[cnt] == *str2++)
+				cnt++;
+		}
+		if (cnt == size)
+			write(1, str1, size);
+	}
+	write(1, "\n", 1);
+}
