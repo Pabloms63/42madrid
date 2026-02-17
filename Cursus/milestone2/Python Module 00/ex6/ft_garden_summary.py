c1537f88d141f01f0fddf8751c96a47c5f6639c1
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-02-17 16:55:43 +0100

    Clases corregidas

--- Cursus/milestone2/Python Module 00/ex6/ft_garden_summary.py
+++ Cursus/milestone2/Python Module 00/ex6/ft_garden_summary.py
@@ -1,4 +1,4 @@
 def ft_garden_summary():
     name: str = input("Enter garden name: ")
-    number: int = int(input("Enter number of plants: "))
+    number: str = (input("Enter number of plants: "))
     print("Garden: ", name, "\nPlants: ", number)
