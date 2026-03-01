f11a882623bb3c5e0cf2818a11fd00558d25ee89
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-03-02 00:47:56 +0100

    Rename function to ft_rrange and update logic

--- Cursus/milestone2/exam_milestone2/level3/ft_range.c
+++ Cursus/milestone2/exam_milestone2/level3/ft_range.c
@@ -1,42 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pmarcos- <pmarcos-@student.42madrid.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/23 22:02:21 by pmarcos-          #+#    #+#             */
-/*   Updated: 2026/02/23 23:15:54 by pmarcos-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 
-int *ft_range(int start, int end)
+int	*ft_rrange(int start, int end)
 {
-    int len;
-    int step;
-    int i = 0;
-    int *arr;
+	int		len;
+	int		*arr;
+	int		i = 0;
 
-    if (start <= end)
-    {
-        len = end - start + 1;
-        step = 1;
-    }
-    else
-    {
-        len = start - end + 1;
-        step = -1;
-    }
-    arr = malloc(sizeof(int) * len);
-    if (!arr)
-        return NULL;
-    while (i < len)
-    {
-        arr[i] = start;
-        start += step;
-        i++;
-    }
-    return arr;
+	if (start <= end)
+		len = end - start + 1;
+	else
+		len = start - end + 1;
+
+	arr = (int *)malloc(sizeof(int) * len);
+	if (!arr)
+		return (NULL);
+
+	while (i < len)
+	{
+		arr[i] = end;
+		if (end > start)
+			end--;
+		else
+			end++;
+		i++;
+	}
+	return (arr);
 }
