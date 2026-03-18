e9dbc017d0085d4dea5244fe0afb34f4d967182d
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-03-18 17:39:47 +0100

    Fix indentation and formatting in ft_ancient_text.py

--- Cursus/milestone2/Python Module 04/ex0/ft_ancient_text.py
+++ Cursus/milestone2/Python Module 04/ex0/ft_ancient_text.py
@@ -1,17 +1,17 @@
 if __name__ == "__main__":
-	print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
-	vault_file = "ancient_fragment.txt"
-	print(f"Accessing storage Vault: {vault_file}")
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    vault_file = "ancient_fragment.txt"
+    print(f"Accessing storage Vault: {vault_file}")
 
-	try:
-		vault = open(vault_file, "r")
-		print("Connection established...")
+    try:
+        vault = open(vault_file, "r")
+        print("Connection established...")
 
-		data =vault.read()
-		print("RECOVERED DATA:")
-		print(data)
+        data = vault.read()
+        print("RECOVERED DATA:")
+        print(data)
 
-		vault.close()
-		print("\nData recovery complete. Storage unit disconnected.")
-	except FileNotFoundError:
-		print("ERROR: Storage vault not found.")
+        vault.close()
+        print("\nData recovery complete. Storage unit disconnected.")
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found.")
