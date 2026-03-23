4a2cdd2e80b6aa860cba2c704425a6782c74e7cb
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-03-23 21:56:49 +0100

    Add files via upload

---
+++ Cursus/milestone2/Python Module 04/ex2/ft_stream_management.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main():
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+
+    archivist_ID = input("Input Stream active. Enter archivist ID: ")
+    status_report = input("Input Stream active. Enter status report: ")
+
+    sys.stdout.write(
+        f"\n[STANDARD] Archive status from {archivist_ID}: {status_report}\n")
+    sys.stderr.write(
+        "[ALERT] System diagnostic: Communication channels verified\n")
+    sys.stdout.write("[STANDARD] Data transmission complete\n")
+
+    print("\nThree-channel communication test successful.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Cursus/milestone2/Python Module 04/ex3/ft_vault_security.py
@@ -0,0 +1,18 @@
+def main():
+    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+    print("Initiating secure vault access...")
+
+    print("Vault connection established with failsafe protocols\n")
+    print("SECURE EXTRACTION:")
+    try:
+        with open("classified_data.txt", "r") as file:
+             for line in file:
+                print(line.strip())
+    except FileNotFoundError:
+        pass
+
+    try:
+
+
+if __name__ == "__main__":
+	main()
