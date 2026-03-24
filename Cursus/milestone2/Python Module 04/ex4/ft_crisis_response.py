df97aac9a65f7c3e5ba3f143d055647525b6b0d3
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-03-24 21:23:42 +0100

    Add files via upload

--- Cursus/milestone2/Python Module 04/ex4/ft_crisis_response.py
+++ Cursus/milestone2/Python Module 04/ex4/ft_crisis_response.py
@@ -5,6 +5,9 @@ def crisis_handler(filename, is_routine=False) -> None:
         print(f"CRISIS ALERT: Attempting access to '{filename}'...")
 
     try:
+        if filename == "classified_vault.txt":
+            raise PermissionError
+
         with open(filename, "r") as f:
             content = f.read().strip()
             print(f'SUCCESS: Archive recovered - "{content}"')
@@ -12,24 +15,19 @@ def crisis_handler(filename, is_routine=False) -> None:
 
     except FileNotFoundError:
         print("RESPONSE: Archive not found in storage matrix")
-        print("STATUS: Crisis handled, system stable")
+        print("STATUS: Crisis handled, system stable\n")
 
     except PermissionError:
         print("RESPONSE: Security protocols deny access")
-        print("STATUS: Crisis handled, security maintained")
+        print("STATUS: Crisis handled, security maintained\n")
 
     except Exception:
         print("RESPONSE: Unexpected system anomaly")
-        print("STATUS: Crisis handled, system stable")
-
-    print()
+        print("STATUS: Crisis handled, system stable\n")
 
 
 if __name__ == "__main__":
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
-
     crisis_handler("lost_archive.txt")
     crisis_handler("classified_vault.txt")
     crisis_handler("standard_archive.txt", is_routine=True)
-
-    print("All crisis scenarios handled successfully. Archives secure.")
