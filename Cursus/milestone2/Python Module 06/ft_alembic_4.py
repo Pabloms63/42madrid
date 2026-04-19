21e7975c7c512044d1863d6c81b63451b2a5f54c
Pablo Marcos Sánchez <122307396+Pabloms63@users.noreply.github.com>
2026-04-19 16:55:52 +0200

    Add files via upload

--- Cursus/milestone2/Python Module 06/ft_alembic_4.py
+++ Cursus/milestone2/Python Module 06/ft_alembic_4.py
@@ -4,9 +4,9 @@ import alchemy
 print("=== Alembic 4 ===")
 print("Accessing the alchemy module using 'import alchemy'")
 print(f"Testing create_air: {alchemy.create_air()}")
-print("Showing not all functions can be reached")
+
+print("Now show that not all functions can be reached")
 print("This will raise an exception!")
-try:
-    print(f"Testing hidden create_earth: {alchemy.create_earth()}")
-except AttributeError as e:
-    print(f"AttributeError: {e}. Did you mean: 'create_air'?")
+
+print("Testing the hidden create_earth:")
+print(f"{alchemy.create_earth()}")
