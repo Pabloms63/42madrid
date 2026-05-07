58ad5ba5b24212272253c31156133e6f2842cde8
Pabloms63 <pmarcosm01@gmail.com>
2026-05-07 13:09:33 +0200

    .

--- Cursus/milestone3/Exam Rank 03/Easy/py_echo_validator.py
+++ Cursus/milestone3/Exam Rank 03/Easy/py_echo_validator.py
@@ -1,8 +1,16 @@
 def echo_validator(text: str) -> bool:
-    text_trimed = text.lower().replace(" ", "")
-    if text_trimed == text_trimed[::-1]:
-        return True
-    return False   
+    text_cleaned = []
+
+    for c in text.lower():
+        if c.isalpha():
+            text_cleaned.append(c)
+
+    text_cleaned = "".join(text_cleaned)
+
+    if not text_cleaned:
+        return False
+    
+    return text_cleaned == text_cleaned[::-1]
 
 if __name__ == "__main__":
 	print(echo_validator("A man a plan a canal Panama"))
