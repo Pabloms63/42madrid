b51067efffbaf42e86a7f2a91c0d13f81c009660
Pabloms63 <pmarcosm01@gmail.com>
2026-05-13 19:04:16 +0200

    .

--- Cursus/milestone3/Exam Rank 03/Difícil/py_string_permutation_checker.py
+++ Cursus/milestone3/Exam Rank 03/Difícil/py_string_permutation_checker.py
@@ -17,8 +17,17 @@ def string_permutation_checker(s1: str, s2: str) -> bool:
         else:
             freq2[char] = 1
 
+    print(freq1, freq2)
     return freq1 == freq2
 
 
 if __name__ == "__main__":
-	print(string_permutation_checker("amor", "roma"))
+    print(string_permutation_checker("abc", "bca"))
+    print(string_permutation_checker("abc", "def"))
+    print(string_permutation_checker("listen", "silent"))
+    print(string_permutation_checker("hello", "bello"))
+    print(string_permutation_checker("", ""))
+    print(string_permutation_checker("a", ""))
+    print(string_permutation_checker("Abc", "abc"))
+    print(string_permutation_checker("a gentleman", "elegant man"))
+
